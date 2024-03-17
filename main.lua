ce5428149351f01326245a86c74264c738a1ffa6
MarioABeppler <mario.a.beppler@gmail.com>
2024-03-17 16:15:36 -0300

    created new files Choice, Node and Main

---
+++ main.lua
@@ -0,0 +1,25 @@
+--[[
+
+    Pseudo Código
+
+    Create Gamedata
+    GameData (Escopo Global / singleton)
+    game
+
+    --Criate / Load Nodes
+    createNode()
+    loadNode() -- Lista com todods os nodes do simulador
+    getinitialNOde() -- Node (um item só)
+
+    -- Motor do Simulador (enginer)
+    mainloop
+        Caso a gente tenho um node ativo (propriedade do Gamedata)
+        print do node
+        mostras escolhas
+        perguntar qual escolha o usuario quer tomar
+        avançar pro proximo node
+        condição de vitoria e derrota
+    end
+
+
+]]
